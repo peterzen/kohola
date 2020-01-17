544a68fb0a07b4cb1a03042a56444a44ceb1c548
Peter Banik <peter@froggle.org>
2020-01-17 03:25:04 +0100

    stub WS streaming example

--- frontend/src/grpc-web/ts/src/index.ts
+++ frontend/src/grpc-web/ts/src/index.ts
@@ -1,7 +1,9 @@
 import {grpc} from "@improbable-eng/grpc-web";
 import {VersionService, WalletService} from "../_proto/api_pb_service";
-import {VersionRequest, AccountsRequest} from "../_proto/api_pb";
+import {VersionRequest, AccountsRequest, AccountNotificationsRequest, AccountNotificationsResponse, TransactionNotificationsRequest} from "../_proto/api_pb";
 
+
+const transport = grpc.WebsocketTransport();
 declare const USE_TLS: boolean;
 const host = USE_TLS ? "https://localhost:8443" : "http://localhost:9090";
 
@@ -41,5 +43,35 @@ function getAccounts() {
   });
 }
 
+
+
+
+function getAccountNotifications() {
+  const request = new TransactionNotificationsRequest();
+  
+  const client = grpc.client(WalletService.TransactionNotifications, {
+    host: 'https://localhost:8443',
+    transport: transport
+  });
+  client.onHeaders((headers: grpc.Metadata) => {
+    console.log("onHeaders", headers);
+  });
+  client.onMessage((message: AccountNotificationsResponse) => {
+    console.log("onMessage", message);
+  });
+  client.onEnd((status: grpc.Code, statusMessage: string, trailers: grpc.Metadata) => {
+    console.log("onEnd", status, statusMessage, trailers);
+  });
+  
+  client.start(new grpc.Metadata({"HeaderTestKey1": "ClientValue1"}));
+  client.send(request);
+  client.finishSend(); // included for completeness, but likely unnecessary as the request is unary
+}
+
+
+
+
+
 getVersion();
 getAccounts();
+getAccountNotifications();
