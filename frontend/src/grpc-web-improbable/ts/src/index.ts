2e5953830428189983ef27aa84419da79e92405f
Peter Banik <peter@froggle.org>
2020-01-18 04:31:46 +0100

    TransactionNotificationsRequest test case

--- frontend/src/grpc-web-improbable/ts/src/index.ts
+++ frontend/src/grpc-web-improbable/ts/src/index.ts
@@ -1,77 +1,95 @@
-import {grpc} from "@improbable-eng/grpc-web";
-import {VersionService, WalletService} from "../_proto/api_pb_service";
-import {VersionRequest, AccountsRequest, AccountNotificationsRequest, AccountNotificationsResponse, TransactionNotificationsRequest} from "../_proto/api_pb";
+import { grpc } from "@improbable-eng/grpc-web";
+import { VersionService, WalletService } from "../_proto/api_pb_service";
+import { VersionRequest, AccountsRequest, AccountNotificationsRequest, AccountNotificationsResponse, TransactionNotificationsRequest, TransactionNotificationsResponse } from "../_proto/api_pb";
 
 
 const transport = grpc.WebsocketTransport();
-declare const USE_TLS: boolean;
-const host = USE_TLS ? "https://localhost:8443" : "http://localhost:9090";
+const wsHost = "https://localhost:8443"
 
 
 function getVersion() {
-  const versionRequest = new VersionRequest();
-  grpc.unary(VersionService.Version, {
-    request: versionRequest,
-    host: host,
-    onEnd: res => {
-      console.log('result', res);
-      const { status, statusMessage, headers, message } = res;
-      console.log("onEnd.status", status, statusMessage);
-      console.log("onEnd.headers", headers);
-      if (status === grpc.Code.OK && message) {
-        console.log("onEnd.message", message.toObject());
-      }
-    }
-  });
+	const versionRequest = new VersionRequest();
+	grpc.unary(VersionService.Version, {
+		request: versionRequest,
+		host: wsHost,
+		onEnd: res => {
+			console.log('result', res);
+			const { status, statusMessage, headers, message } = res;
+			console.log("onEnd.status", status, statusMessage);
+			console.log("onEnd.headers", headers);
+			if (status === grpc.Code.OK && message) {
+				console.log("onEnd.message", message.toObject());
+			}
+		}
+	});
 }
 
 
 function getAccounts() {
-  const accountRequest = new AccountsRequest();
-  grpc.unary(WalletService.Accounts, {
-    request: accountRequest,
-    host: host,
-    onEnd: res => {
-      console.log('result', res);
-      const { status, statusMessage, headers, message } = res;
-      console.log("onEnd.status", status, statusMessage);
-      console.log("onEnd.headers", headers);
-      if (status === grpc.Code.OK && message) {
-        console.log("onEnd.message", message.toObject());
-      }
-    }
-  });
+	const accountRequest = new AccountsRequest();
+	grpc.unary(WalletService.Accounts, {
+		request: accountRequest,
+		host: wsHost,
+		onEnd: res => {
+			console.log('result', res);
+			const { status, statusMessage, headers, message } = res;
+			console.log("onEnd.status", status, statusMessage);
+			console.log("onEnd.headers", headers);
+			if (status === grpc.Code.OK && message) {
+				console.log("onEnd.message", message.toObject());
+			}
+		}
+	});
 }
 
 
 
 
 function getAccountNotifications() {
-  const request = new TransactionNotificationsRequest();
-  
-  const client = grpc.client(WalletService.TransactionNotifications, {
-    host: 'https://localhost:8443',
-    transport: transport
-  });
-  client.onHeaders((headers: grpc.Metadata) => {
-    console.log("onHeaders", headers);
-  });
-  client.onMessage((message: AccountNotificationsResponse) => {
-    console.log("onMessage", message);
-  });
-  client.onEnd((status: grpc.Code, statusMessage: string, trailers: grpc.Metadata) => {
-    console.log("onEnd", status, statusMessage, trailers);
-  });
-  
-  client.start(new grpc.Metadata({"HeaderTestKey1": "ClientValue1"}));
-  client.send(request);
-  client.finishSend(); // included for completeness, but likely unnecessary as the request is unary
+	const request = new AccountNotificationsRequest();
+
+	const client = grpc.client(WalletService.AccountNotifications, {
+		host: wsHost,
+		transport: transport
+	});
+	client.onHeaders((headers: grpc.Metadata) => {
+		console.log("onHeaders", headers);
+	});
+	client.onMessage((message: AccountNotificationsResponse) => {
+		console.log("accountnotifications", message.toObject());
+	});
+	client.onEnd((status: grpc.Code, statusMessage: string, trailers: grpc.Metadata) => {
+		console.log("onEnd", status, statusMessage, trailers);
+	});
+
+	client.start(new grpc.Metadata({ "HeaderTestKey1": "ClientValue1" }));
+	client.send(request);
 }
 
 
+function getTxNotifications() {
+	const request = new TransactionNotificationsRequest();
 
+	const client = grpc.client(WalletService.TransactionNotifications, {
+		host: wsHost,
+		transport: transport
+	});
+	client.onHeaders((headers: grpc.Metadata) => {
+		console.log("onHeaders", headers);
+	});
+	client.onMessage((message: TransactionNotificationsResponse) => {
+		console.log("txnotifications", message.toObject());
+	});
+	client.onEnd((status: grpc.Code, statusMessage: string, trailers: grpc.Metadata) => {
+		console.log("onEnd", status, statusMessage, trailers);
+	});
+
+	client.start(new grpc.Metadata({ "HeaderTestKey1": "ClientValue1" }));
+	client.send(request);
+}
 
 
 getVersion();
 getAccounts();
 getAccountNotifications();
+getTxNotifications();
