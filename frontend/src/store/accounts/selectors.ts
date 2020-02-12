a171fdee05267a6399a5c0a185e3b1b22f02e0a1
Peter Banik <peter@froggle.org>
2020-02-12 02:09:05 +0100

    hide imported account

--- frontend/src/store/accounts/selectors.ts
+++ frontend/src/store/accounts/selectors.ts
@@ -3,11 +3,12 @@ import { IApplicationState } from "../types"
 import { IndexedWalletAccounts, WalletAccount } from "../../models"
 
 export const getAccounts = (state: IApplicationState): IndexedWalletAccounts => {
-	return state.accounts.accounts
+	return _.filter(state.accounts.accounts, (r)=>r.getAccountName()!="imported")
 }
 
 export const getAllAccountNumbers = (state: IApplicationState): number[] => {
 	return _.chain(state.accounts.accounts)
+		.filter((r)=>r.getAccountName()!="imported")
 		.keys()
 		.map((s) => parseInt(s))
 		.value()
