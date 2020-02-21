3df9a53a0e1aaf5867e9a42e75a1a7ba1c0036cf
Peter Banik <peter@froggle.org>
2020-02-21 03:01:13 +0100

    cleanup

--- frontend/src/components/Fixtures/DevTools.tsx
+++ frontend/src/components/Fixtures/DevTools.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 
-// Exported from redux-devtools
-import { createDevTools } from 'redux-devtools';
-
-// Monitors are separate packages, and you can make a custom one
 import LogMonitor from 'redux-devtools-log-monitor';
 import DockMonitor from 'redux-devtools-dock-monitor';
-// import SliderMonitor from 'redux-devtools-slider-monitor';
 import SliderMonitor from 'redux-slider-monitor';
+
+import { createDevTools } from 'redux-devtools';
 
 const DevTools = createDevTools(
 	<DockMonitor
@@ -18,7 +15,9 @@ const DevTools = createDevTools(
 		defaultIsVisible={true}
 	>
 		<SliderMonitor keyboardEnabled/>
-		<LogMonitor theme="tomorrow" />
+		<LogMonitor
+			theme="tomorrow"
+		/>
 	</DockMonitor>
 );
 
