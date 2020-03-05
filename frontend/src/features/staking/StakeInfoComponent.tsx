30bdd81410848e68c47eaf4e5284f82771e96626
Peter Banik <peter@froggle.org>
2020-03-05 14:48:54 +0100

    added total count, mapDispatch cleanup

--- frontend/src/features/staking/StakeInfoComponent.tsx
+++ frontend/src/features/staking/StakeInfoComponent.tsx
@@ -6,51 +6,37 @@ import { IApplicationState } from "../../store/store";
 import { TicketStatusIcon } from "./TicketStatusIcon";
 import { TicketStatus } from "../../constants";
 import { loadStakeInfoAttempt, IStakeInfoState } from "./stakingSlice";
-import { bindActionCreators, Dispatch } from "@reduxjs/toolkit";
+import { StakeInfo } from "../../models";
 
-
-class StakeInfoComponent extends React.Component<Props, InternalState> {
+class StakeInfoComponent extends React.Component<Props> {
 	render() {
 		const s = this.props.stakeinfo;
+		const allTixCount = s.getVoted() + s.getLive() + s.getImmature() + s.getOwnMempoolTix() + s.getMissed()
 		return (
 			<Card>
 				<Card.Body>
 					<Row>
 						<Col>
-							<h2>{s.getVoted()}</h2>
-							<h6 className="text-muted"><TicketStatusIcon status={TicketStatus.VOTED} /> Voted recently</h6>
+							<h2>{allTixCount}</h2>
+							<h6 className="text-muted">Σ Total</h6>
 						</Col>
 						<Col>
-							<h2>{s.getLive()}</h2>
-							<h6 className="text-muted"><TicketStatusIcon status={TicketStatus.LIVE} /> Live</h6>
+							<h2>{s.getOwnMempoolTix()}</h2>
+							<h6 className="text-muted"><TicketStatusIcon status={TicketStatus.UNMINED} /> In mempool</h6>
 						</Col>
 						<Col>
 							<h2>{s.getImmature()}</h2>
 							<h6 className="text-muted"><TicketStatusIcon status={TicketStatus.IMMATURE} /> Immature</h6>
 						</Col>
 						<Col>
-							<h2>{s.getOwnMempoolTix()}</h2>
-							<h6 className="text-muted"><TicketStatusIcon status={TicketStatus.UNMINED} /> Mempool</h6>
+							<h2>{s.getLive()}</h2>
+							<h6 className="text-muted"><TicketStatusIcon status={TicketStatus.LIVE} /> Live</h6>
+						</Col>
+						<Col>
+							<h2>{s.getVoted()}</h2>
+							<h6 className="text-muted"><TicketStatusIcon status={TicketStatus.VOTED} /> Voted recently</h6>
 						</Col>
 					</Row>
-					{/* <Row>
-					<Col>
-							<h1>{s.getVoted()}</h1>
-							<h6 className="text-muted"><TicketStatusIcon status={TicketStatus.VOTED} /> Voted</h6>
-						</Col>
-						<Col>
-							<h1>{s.getExpired()}</h1>
-							<h6 className="text-muted"><TicketStatusIcon status={TicketStatus.EXPIRED} /> Expired</h6>
-						</Col>
-						<Col>
-							<h1>{s.getMissed()}</h1>
-							<h6 className="text-muted"><TicketStatusIcon status={TicketStatus.MISSED} /> Missed</h6>
-						</Col>
-						<Col>
-							<h1>{s.getRevoked()}</h1>
-							<h6 className="text-muted"><TicketStatusIcon status={TicketStatus.REVOKED} /> Revoked</h6>
-						</Col>
-					</Row> */}
 				</Card.Body>
 			</Card>
 		)
@@ -62,26 +48,23 @@ class StakeInfoComponent extends React.Component<Props, InternalState> {
 
 
 interface OwnProps {
+	stakeinfo: StakeInfo
 }
 
 interface DispatchProps {
-	loadStakeInfoAttempt: () => void
+	loadStakeInfoAttempt: typeof loadStakeInfoAttempt
 }
 
-type Props = IStakeInfoState & DispatchProps & OwnProps
+type Props = DispatchProps & OwnProps
 
-interface InternalState {
-}
-
-const mapStateToProps = (state: IApplicationState, ownProps: OwnProps): IStakeInfoState => {
+const mapStateToProps = (state: IApplicationState): OwnProps => {
 	return {
-		...state.staking,
 		stakeinfo: state.staking.stakeinfo,
 	};
 }
 
-const mapDispatchToProps = (dispatch: Dispatch) => bindActionCreators({
-	loadStakeInfoAttempt: loadStakeInfoAttempt
-}, dispatch)
+const mapDispatchToProps = {
+	loadStakeInfoAttempt
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(StakeInfoComponent)
