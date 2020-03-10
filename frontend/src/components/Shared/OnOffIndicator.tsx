1e3940f8c7cafed6a93b0038a0fdf09e795704e3
Peter Banik <peter@froggle.org>
2020-03-10 18:12:13 +0100

    OnOffIndicator shows textual message

--- frontend/src/components/Shared/OnOffIndicator.tsx
+++ frontend/src/components/Shared/OnOffIndicator.tsx
@@ -3,22 +3,41 @@ import * as React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircle } from '@fortawesome/free-solid-svg-icons'
 import { SizeProp } from "@fortawesome/fontawesome-svg-core";
+// @ts-ignore
+import Fade from 'react-reveal/Fade';
 
 const OnOffIndicator = (props: Props) => {
 
 	const size = props.size || "1x"
 
 	return (
-		<span>
-			{props.status === true && <FontAwesomeIcon icon={faCircle} className="text-success" size={size} />}
-			{props.status === false && <FontAwesomeIcon icon={faCircle} className="text-danger" size={size} />}
-			{props.status === undefined && <FontAwesomeIcon icon={faCircle} className="text-secondary" size={size} />}
-		</span>
+		<Fade fade>
+			<span>
+				{props.status === true && (
+					<span>
+						<FontAwesomeIcon icon={faCircle} className="text-success" size={size} /> {props.onMessage}
+					</span>
+				)}
+				{props.status === false && (
+					<span>
+						<FontAwesomeIcon icon={faCircle} className="text-danger" size={size} /> {props.offMessage}
+					</span>
+				)}
+				{props.status === undefined && (
+					<span>
+						<FontAwesomeIcon icon={faCircle} className="text-secondary" size={size} /> {props.undefMessage}
+					</span>
+				)}
+			</span>
+		</Fade>
 	)
 }
 
 interface Props {
 	status: boolean | undefined
+	onMessage?: string
+	offMessage?: string
+	undefMessage?: string
 	size?: SizeProp
 }
 
