3515d2791c38270258a11910aa9d5fdd810f067c
Peter Banik <peter@froggle.org>
2020-03-26 22:21:28 +0100

    Fixed typo and added Test_fetchStakeDiffHistory

--- app/dcrdata_test.go
+++ app/dcrdata_test.go
@@ -1,28 +1,47 @@
 package main
 
 import (
-	"fmt"
+	"reflect"
 	"testing"
+
+	gui "github.com/peterzen/dcrwalletgui/walletgui"
 )
 
-func Test_queryDcrdata(t *testing.T) {
+func Test_fetchStakeDiffHistory(t *testing.T) {
+	type args struct {
+		startBlock uint32
+		endBlock   uint32
+	}
 	tests := []struct {
-		name string
+		name        string
+		args        args
+		wantHistory *gui.StakeDiffHistory
+		wantErr     bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "bestblock",
+			args: args{
+				startBlock: 433000,
+				endBlock:   433001,
+			},
+			wantHistory: &gui.StakeDiffHistory{
+				SdiffValues: []float32{135.54547, 135.54547},
+				StartBlock:  433000,
+				EndBlock:    433001,
+			},
+			wantErr: false,
+		},
 	}
-	_ = tests
-	sdiffHistory, err := fetchStakeDiffHistory(433000, 434000)
-	if err != nil {
-		return
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotHistory, err := fetchStakeDiffHistory(tt.args.startBlock, tt.args.endBlock)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("fetchStakeDiffHistory() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotHistory, tt.wantHistory) {
+				t.Errorf("fetchStakeDiffHistory() = %v, want %v", gotHistory, tt.wantHistory)
+			}
+		})
 	}
-
-	fmt.Printf("%v", sdiffHistory)
-
-	// for _, tt := range tests {
-	// 	t.Run(tt.name, func(t *testing.T) {
-	// 		queryDcrdata()
-	// 	})
-	// }
 }
-6
