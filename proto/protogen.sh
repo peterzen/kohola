e1e2dffe1d69cb28450453e797678dccb94a8585
Peter Banik <peter@froggle.org>
2020-04-04 02:42:05 +0200

    Generate api.proto as well

--- proto/protogen.sh
+++ proto/protogen.sh
@@ -11,3 +11,9 @@ protoc  \
     --ts_out=service=grpc-web:$JS_OUTPUT_DIR \
 	--go_out=$GO_OUTPUT_DIR
 
+protoc  \
+    --plugin=protoc-gen-ts=../frontend/node_modules/.bin/protoc-gen-ts \
+    -I=$PROTO_DIR $PROTO_DIR/api.proto \
+    --js_out=import_style=commonjs,binary:$JS_OUTPUT_DIR \
+    --ts_out=service=grpc-web:$JS_OUTPUT_DIR \
+
